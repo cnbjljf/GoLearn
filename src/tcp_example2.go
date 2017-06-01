44d76f1759848850d0722f3cf5e78a93dc488e2d
Leo <1403208717@qq.com>
2017-06-01 12:03:10 +0800

    raw func of Dial

---
+++ src/tcp_example2.go
@@ -0,0 +1,38 @@
+// tcp_simple2
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Error", os.Stderr)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port", os.Args[0])
+		os.Exit(1)
+	}
+	service := os.Args[1]
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	checkError(err)
+
+	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	checkError(err)
+
+	result, err := ioutil.ReadAll(conn)
+	checkError(err)
+
+	fmt.Println(string(result))
+	os.Exit(1)
+}
