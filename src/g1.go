489074e9137c8bcccec8607d4ddf0466fa6042d3
Leo <1403208717@qq.com>
2017-06-26 18:46:17 +0800

    new pratice

--- src/g1.go
+++ src/g1.go
@@ -3,8 +3,38 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+
+	aa := "14+12"
+	bb := strings.Split(aa, "+")
+	f1 := []rune(bb[0])
+	f2 := []rune(bb[1])
+
+	lenf1 := len(f1)
+	lenf2 := len(f2)
+	var lenMax int
+	//	var lenMin int
+
+	left := 0
+
+	if lenf1 >= lenf2 {
+		//		lenMin = lenf2
+		lenMax = lenf1
+	} else {
+		//		lenMin = lenf1
+		lenMax = lenf2
+	}
+	for i := 1; i <= lenMax; i++ {
+		index := lenMax - i
+		sum := (f1[index] - '0') + (f2[index] - '0') + rune(left)
+		fmt.Println(sum)
+		if sum > 10 {
+			left = 1
+		}
+	}
+
+	fmt.Printf("%c %c", f1, f2)
 }
