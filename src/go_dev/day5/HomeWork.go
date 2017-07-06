4c4b8c7e06748d3c66ee0d9e3cd5ebc9405321f0
Leo <1403208717@qq.com>
2017-07-06 08:29:59 +0800

    writing homework

--- src/go_dev/day5/HomeWork.go
+++ src/go_dev/day5/HomeWork.go
@@ -29,12 +29,24 @@ type student struct {
 }
 
 func showBook(b *book) {
+	// 显示书籍的
 	for b != nil {
 		fmt.Println("book name", b.name, ", book author", b.author, ", book published",
 			b.published, ", how many book", b.many, ", the next book", b.next)
 
 		b = b.next
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func showStu(s *student) {
+	// 显示学生信息的
+	for s != nil {
+		fmt.Println("student's name", s.name, ", student's sex", s.sex, ", student's ID",
+			s.ID, "the next student", s.next)
+
+		s = s.next
+		time.Sleep(10 * time.Millisecond)
 	}
 }
 
@@ -46,6 +58,7 @@ func addBook(b *book) book {
 	for {
 		fmt.Println("please input the book's basic info,enter quit will quit this func, and the format is ")
 		fmt.Println(" name, author ,published , how many")
+		fmt.Println("举个例子：    aaa,Leo,20170705,12")
 		reader := bufio.NewReader(os.Stdin)
 		result, _, err := reader.ReadLine()
 		if err != nil {
@@ -100,38 +113,40 @@ func selectBook(b *book, key string) {
 	for b != nil {
 		if b.name == key || b.author == key || b.published == key {
 			fmt.Println("find this book ,the detail of the book will show you ")
-			fmt.Println("the book name", b.name, ", the book author", b.author, ", the book published",
-				b.published, ",and how many book", b.many)
+			fmt.Println("the book name is [", b.name, "], the book author is[", b.author, "], the book published is[",
+				b.published, "],and we have [", b.many, "] book")
 		}
 		b = b.next
 	}
 	//	}
 }
 
-func addStudent(s *student) *student {
+func addStudent(s *student) student {
 	// 录入学生信息的
 	var st = s
 	var name, ID, sex string
 
 	for {
+		fmt.Println("please input student's info,enter quit is exit,and the format is name,ID,sex    ")
+		fmt.Println("必须以逗号分隔！！！例如：   ljf,2017070501,man")
 		reader := bufio.NewReader(os.Stdin)
 		rt, _, err := reader.ReadLine()
 		if err != nil {
 			fmt.Println("happend a error: ", err)
 			continue
 		}
-		rt = strings.Split(string(rt), ",")
-		name = strings.TrimSpace(rt[0])
+		result := strings.Split(string(rt), ",")
+		name = strings.TrimSpace(result[0])
 		if name == "quit" {
 			fmt.Println("exit to add student's info")
 			break
 		}
-		if len(rt) != 3 {
+		if len(result) != 3 {
 			fmt.Println("you weren't provide right arguments")
 			continue
 		}
-		ID = strings.TrimSpace(rt[1])
-		sex = strings.TrimSpace(rt[2])
+		ID = strings.TrimSpace(result[1])
+		sex = strings.TrimSpace(result[2])
 		stu := student{
 			name: name,
 			ID:   ID,
@@ -140,7 +155,24 @@ func addStudent(s *student) *student {
 		st.next = &stu
 		st = &stu
 	}
-	return s
+	return *s
+}
+
+func borrowBook(b *book, s *student) student {
+	// 借书的功能，主要修改学生的信息
+	var i int
+	for b != nil {
+		i++
+		fmt.Printf("%d -- %s[author:%s,left:%d]\n", i, b.name, b.author, b.many)
+	}
+	fmt.Println("please input which book do you want to borrow, you can type the serial num or book's name")
+	reader := bufio.NewReader(os.Stdin)
+	result, _, err := reader.ReadLine()
+	if err != nil {
+		fmt.Println("happend a error!", err)
+	}
+	rt := strings.Split(string(result), ",")
+	return *s
 }
 
 func main() {
@@ -150,6 +182,13 @@ func main() {
 		published: "1986-07-01",
 		many:      12,
 	}
+
+	stuInit := student{
+		name: "admin",
+		sex:  "man",
+		ID:   "2017070501",
+	}
+
 	for {
 		msg := `
 1： add books
@@ -187,6 +226,10 @@ func main() {
 			selectBook(&bInit, key)
 
 		case 3:
+			stuInit = addStudent(&stuInit)
+			showStu(&stuInit)
+		case 4:
+			borrowBook(&bInit, &stuInit)
 
 		default:
 			fmt.Println("you weren't input a available choice!!")
